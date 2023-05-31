5299a30b772f87af9325cac36b1796f7bc53eb52
asadulladev <usmanbeliever@gmail.com>
2023-05-31 16:51:34 +0600

    humburger

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -65,6 +65,9 @@ const Search = ({ tabs }) => {
         <div id="search">
             <div className="container">
                 <div className="search">
+                    <div className="search--button" onClick={handleSearch}>
+                    <MdSearch />
+                </div>
                     <input
                         type="text"
                         value={searchQuery}
@@ -72,9 +75,7 @@ const Search = ({ tabs }) => {
                         className="search-input"
                         placeholder="Поиск"
                     />
-                    <button className="search-button" onClick={handleSearch}>
-                        <MdSearch />
-                    </button>
+
                     {filteredCards?.map((card, index) => (
                         <div className="card" key={index}>
                             {/* Render the filtered cards */}
